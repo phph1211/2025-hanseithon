40091db5d19a6081c814f5f1467cd0f79de1ab38
박한 <bagkan1211@gmail.com>
2025-07-23 13:58:44 +0900

    fix: left time

--- src/pages/countdown/index.tsx
+++ src/pages/countdown/index.tsx
@@ -8,7 +8,7 @@ export default function CountDownPage() {
     const target = new Date();
 
     target.setDate(now.getDate() + 1);
-    target.setHours(14, 0, 0, 0);
+    target.setHours(11, 30, 0, 0);
 
     const diffInSeconds = Math.floor((target.getTime() - now.getTime()) / 1000);
     return diffInSeconds;
